215b36be49875ca754b196026c256d3c9df794c3
sanemat <o.gata.ken@gmail.com>
2013-03-30 17:20:16 +0900

    Add version
    via bundler lib/bundler/cli.rb

--- lib/mami/cli.rb
+++ lib/mami/cli.rb
@@ -3,9 +3,10 @@ require 'thor'
 
 module Mami
   class CLI < Thor
-    desc 'echo mami', 'echo mami name'
-    def name
-      puts 'Tomoe Mami'
+    desc "version", "Prints the mami's version information"
+    def version
+      puts "Mami version #{Mami::VERSION}"
     end
+    map %w(-v --version) => :version
   end
 end
