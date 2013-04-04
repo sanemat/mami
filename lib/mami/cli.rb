e7df2dc46f8dbe7f41d13ab1fe30a6f72cede835
sanemat <o.gata.ken@gmail.com>
2013-04-04 19:35:39 +0900

    Use abort method

--- lib/mami/cli.rb
+++ lib/mami/cli.rb
@@ -16,7 +16,7 @@ module Mami
     method_option :directory, default: nil, aliases: '-d', desc: 'Set directory. if nil, then use MAMI_DIR'
     def print
       path = options[:directory] || ENV['MAMI_DIR']
-      return puts "no-mami" unless path
+      abort("mami requires MAMI_DIR or directory option") unless path
       fullpath = File.expand_path(path)
       basename = %x[date "+%Y-%m-%d-%H-%M-%S"].chomp
       extension = options[:extension].nil? ? nil : '.' + options[:extension]
