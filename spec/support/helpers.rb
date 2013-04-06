b05a67b757c040ba7b0241f517dc06e0e9a74d40
sanemat <o.gata.ken@gmail.com>
2013-04-07 03:32:47 +0900

    Copy from bundler/spec/support/helpers.rb(sevral methods)

---
+++ spec/support/helpers.rb
@@ -0,0 +1,31 @@
+# Copy from bundler/spec/support/helpers.rb
+module Spec
+  module Helpers
+    def reset!
+      @in_p, @out_p, @err_p = nil, nil, nil
+    end
+
+    attr_reader :out, :err, :exitstatus
+
+    def sys_exec(cmd, expect_err = false)
+      Open3.popen3(cmd.to_s) do |stdin, stdout, stderr|
+        @in_p, @out_p, @err_p = stdin, stdout, stderr
+
+        yield @in_p if block_given?
+        @in_p.close
+
+        @out = @out_p.read_available_bytes.strip
+        @err = @err_p.read_available_bytes.strip
+      end
+
+      puts @err unless expect_err || @err.empty? || !$show_err
+      @out
+    end
+
+    def sys_status(cmd)
+      @err = nil
+      @out = %x{#{cmd}}.strip
+        @exitstatus = $?.exitstatus
+    end
+  end
+end
