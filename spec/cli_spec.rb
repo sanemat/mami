bb35f171b784c0110ab7c46d7ee5276afd49d762
sanemat <o.gata.ken@gmail.com>
2013-04-09 19:30:06 +0900

    Add golden path

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -6,3 +6,10 @@ describe 'bundle executable' do
     expect(err).to include('Unknown switches')
   end
 end
+
+describe 'common case' do
+  it 'returns common output' do
+    mami ''
+    expect(out).to eq('/path/to/mami/2011-08-28-12-34-56.txt')
+  end
+end
