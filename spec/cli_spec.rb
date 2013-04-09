13b2f0dfce704f63c59190b39d884638342ee705
sanemat <o.gata.ken@gmail.com>
2013-04-09 19:41:52 +0900

    Add MAMI_DIR on before block

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -8,6 +8,10 @@ describe 'bundle executable' do
 end
 
 describe 'common case' do
+  before do
+    ENV['MAMI_DIR'] = "/path/to/mami"
+  end
+
   it 'returns common output' do
     mami ''
     expect(out).to eq('/path/to/mami/2011-08-28-12-34-56.txt')
